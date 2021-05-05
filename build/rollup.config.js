b2673e13b53c3b6cd4eb8441b2c114980b14bcfd
Joe Timmer <profsmallpine@gmail.com>
2021-05-05 11:31:31 -0600

    Fixes build issue

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -46,6 +46,7 @@ const baseConfig = {
     },
     postVue: [
       resolve({
+        browser: true,
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
       }),
       // Process only `<style module>` blocks.
