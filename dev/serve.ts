9c7de65ee669ace8a51f642f4fd4a1944cba75ba
Joe Timmer <profsmallpine@gmail.com>
2021-06-01 16:21:59 -0600

    Fixes case issue

--- dev/serve.ts
+++ dev/serve.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import Serve from "./Serve.vue";
+import Serve from "./serve.vue";
 // To register individual components where they are used (serve.vue) instead of using the
 // library as a whole, comment/remove this import and it's corresponding "app.use" call
 import Trees from "@/entry.esm";
