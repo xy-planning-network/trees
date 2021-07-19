9b08d13f3d6d07a4ab316b8fd6e3cb3101857d12
Joe Timmer <profsmallpine@gmail.com>
2021-07-19 09:46:39 -0600

    Updates download cell to use svg component

--- src/lib-components/lists/DownloadCell.vue
+++ src/lib-components/lists/DownloadCell.vue
@@ -1,24 +1,14 @@
 <template>
   <a :href="propsData[attribute]">
-    <svg
-      class="h-6 w-6 group-hover:text-gray-500 transition"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-        clip-rule="evenodd"
-      />
-    </svg>
+    <DownloadIcon class="h-6 w-6 group-hover:text-gray-500 transition" />
   </a>
 </template>
 
 <script lang="ts">
 import { Options, Prop, Vue } from "vue-property-decorator";
+import { DownloadIcon } from "@heroicons/vue/solid";
 
-@Options({ name: "DownloadCell" })
+@Options({ name: "DownloadCell", components: { DownloadIcon } })
 export default class DownloadCell extends Vue {
   @Prop({ type: Object, required: true }) propsData!: Record<string, unknown>;
   @Prop({ type: String, required: true }) attribute!: string;
