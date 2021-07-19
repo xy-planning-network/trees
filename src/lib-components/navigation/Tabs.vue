9f8663c11504d3cb06b0b22c007d262134f8928c
Joe Timmer <profsmallpine@gmail.com>
2021-07-19 13:25:00 -0600

    Adds pill design option to tabs

--- src/lib-components/navigation/Tabs.vue
+++ src/lib-components/navigation/Tabs.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="sm:hidden mb-4">
+    <div class="sm:hidden" :class="{ pillDesign: 'mb-4' }">
       <label for="tabs" class="sr-only">Select a tab</label>
       <Select
         name="tabs"
@@ -10,23 +10,19 @@
       />
     </div>
     <div class="hidden sm:block">
-      <nav class="flex ml-8">
-        <a
-          href="#"
-          class="px-12 py-2 font-medium text-md leading-5 rounded-t-md focus:outline-none"
-          v-for="(tab, idx) in tabs"
-          :key="idx"
-          v-text="tab.label"
-          @click.prevent="updateModelValue(tab.value)"
-          :class="{
-            'focus:bg-white text-gray-700 bg-white border-b-2 border-blue-500':
-              tab.value === modelValue,
-            'text-gray-500 hover:text-gray-700 focus:text-gray-700 focus:bg-gray-100 border border-gray-200':
-              tab.value != modelValue,
-          }"
-        >
-        </a>
-      </nav>
+      <div :class="{ notPillDesign: 'border-b border-gray-200' }">
+        <nav class="flex" :class="[pillDesign ? 'ml-8' : '-mb-px']">
+          <a
+            href="#"
+            :class="classes(tab.value, idx > 0)"
+            v-for="(tab, idx) in tabs"
+            :key="idx"
+            v-text="tab.label"
+            @click.prevent="updateModelValue(tab.value)"
+          >
+          </a>
+        </nav>
+      </div>
     </div>
   </div>
 </template>
@@ -36,15 +32,57 @@ import { Emit, Options, Prop, Vue } from "vue-property-decorator";
 
 @Options({ name: "Tabs" })
 export default class Tabs extends Vue {
+  @Prop({ type: String, required: true }) modelValue!: string;
+  @Prop({ type: Boolean, required: true }) pillDesign?: boolean;
   @Prop({ type: Array, required: true }) tabs!: Array<{
     label: string;
     value: string;
   }>;
-  @Prop({ type: String, required: true }) modelValue!: string;
 
   @Emit("update:modelValue")
   updateModelValue(modelValue: string): string {
     return modelValue;
+  }
+
+  classes(currentTab: string, pastFirstTab: boolean): string {
+    let c = "";
+
+    if (this.pillDesign) {
+      c =
+        "px-12 py-2 font-medium text-md leading-5 rounded-t-md focus:outline-none ";
+
+      if (this.modelValue === currentTab) {
+        c =
+          c +
+          "focus:bg-white text-gray-700 bg-white border-b-2 border-blue-500";
+      } else {
+        c =
+          c +
+          "text-gray-500 hover:text-gray-700 focus:text-gray-700 focus:bg-gray-100 border border-gray-200";
+      }
+
+      return c;
+    }
+
+    c =
+      "px-1 py-4 text-sm font-medium border-b-2 whitespace-nowrap leading-5 focus:outline-none ";
+    if (this.modelValue === currentTab) {
+      c =
+        c +
+        "border-blue-500 text-xy-blue focus:text-blue-800 focus:border-blue-700";
+    } else {
+      c =
+        c +
+        "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300";
+    }
+
+    if (pastFirstTab) c = c + " ml-8";
+
+    return c;
+  }
+
+  get notPillDesign(): boolean {
+    return !this.pillDesign;
   }
 }
 </script>
