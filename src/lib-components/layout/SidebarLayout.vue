4cfa01e447df6a2b0bc4c9db6066779480bbebbe
David Ketch <david@xyplanningnetwork.com>
2021-08-05 09:57:04 -0500

    src/lib-components/layout/SidebarLayout: if not item.openInTab, target='_self'

--- src/lib-components/layout/SidebarLayout.vue
+++ src/lib-components/layout/SidebarLayout.vue
@@ -65,7 +65,7 @@
                       : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
                     'group flex items-center px-2 py-2 text-base font-medium rounded-md',
                   ]"
-                  :target="item.openInTab ? '_blank' : false"
+                  :target="item.openInTab ? '_blank' : '_self'"
                 >
                   <component
                     :is="item.icon"
@@ -111,7 +111,7 @@
                     : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
                   'group flex items-center px-2 py-2 text-sm font-medium rounded-md',
                 ]"
-                :target="item.openInTab ? '_blank' : false"
+                :target="item.openInTab ? '_blank' : '_self'"
               >
                 <component
                   :is="item.icon"
