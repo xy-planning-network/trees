71d955299dc4c2444106e1d1c738c3cee14b7edf
Steve Klein <steven@xyplanningnetwork.com>
2021-10-19 15:04:41 -0500

    navigation colors for a11y contrast

--- src/lib-components/layout/StackedLayout.vue
+++ src/lib-components/layout/StackedLayout.vue
@@ -15,8 +15,8 @@
                 :class="[
                   isActive(item.url)
                     ? 'border-blue-500 text-gray-900'
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                  'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium',
+                    : 'border-transparent text-gray-700 hover:text-gray-900 hover:border-blue-500',
+                  'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-semibold',
                 ]"
                 :aria-current="isActive(item.url) ? 'page' : undefined"
                 >{{ item.name }}</a
@@ -54,7 +54,7 @@
                       :href="item.url"
                       :class="[
                         active ? 'bg-gray-100' : '',
-                        'block px-4 py-2 text-sm text-gray-700',
+                        'block px-4 py-2 text-sm text-gray-700 font-semibold',
                       ]"
                       >{{ item.name }}</a
                     >
@@ -85,8 +85,8 @@
             :class="[
               isActive(item.url)
                 ? 'bg-blue-50 border-blue-500 text-blue-700'
-                : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
-              'block pl-3 pr-4 py-2 border-l-4 text-base font-medium',
+                : 'border-transparent text-gray-700 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-900',
+              'block pl-3 pr-4 py-2 border-l-4 text-base font-semibold',
             ]"
             :aria-current="isActive(item.url) ? 'page' : undefined"
             >{{ item.name }}</a
@@ -113,7 +113,7 @@
               v-for="item in userNavigation"
               :key="item.name"
               :href="item.url"
-              class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+              class="block px-4 py-2 text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100"
               >{{ item.name }}</a
             >
           </div>
