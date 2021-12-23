a40938a4481abe83d370e037caab004c54a5cbec
Steve Klein <steven@xyplanningnetwork.com>
2021-12-23 13:09:12 -0600

    jit mode for tailwind during dev

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
-const baseConfig = require("./config/tailwind.config");
+const baseConfig = require("./config/tailwind.config")
 module.exports = {
   ...baseConfig,
+  mode: "jit",
   purge: [...baseConfig.purge, "./dev/**/*.vue"],
-};
+}
