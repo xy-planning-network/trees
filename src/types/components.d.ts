07301477a174335d6ba08c7fdb9d7195e671d26c
Steve Klein <steven@xyplanningnetwork.com>
2022-01-03 20:41:09 -0600

    types to their own directory with index for exported types

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,5 +1,7 @@
 import { TreesComponents } from "@/lib-components"
 
+export default TreesComponents
+
 declare module "@vue/runtime-core" {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface GlobalComponents extends TreesComponents {}
---
+++ src/types/index.d.ts
@@ -0,0 +1,3 @@
+export * as NavTypes from "./nav"
+export * as TableTypes from "./table"
+export * as UserTypes from "./users"
