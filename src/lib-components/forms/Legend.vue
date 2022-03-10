35acaef6b54ed577010632e09be0d6016caac5cb
Steve Klein <steven@xyplanningnetwork.com>
2022-03-10 11:45:23 -0600

    feat: basic legend component

---
+++ src/lib-components/forms/Legend.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    text?: string
+  }>(),
+  {
+    text: "",
+  }
+)
+</script>
+
+<template>
+  <legend
+    v-if="text"
+    v-bind="{
+      ...$attrs,
+      class: 'text-base font-medium leading-tight text-gray-900',
+    }"
+  >
+    {{ text }}
+  </legend>
+</template>
