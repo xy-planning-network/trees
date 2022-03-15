791ca6921e86428da76b20bf1d4714df144b239a
Steve Klein <steven@xyplanningnetwork.com>
2022-03-15 13:19:32 -0500

    help and legend support for yes no radio

--- src/lib-components/forms/YesOrNoRadio.vue
+++ src/lib-components/forms/YesOrNoRadio.vue
@@ -6,11 +6,13 @@ import InputLabel from "./InputLabel.vue"
 const props = withDefaults(
   defineProps<{
     modelValue?: boolean
+    help?: string
     legend?: string
     name?: string
   }>(),
   {
     modelValue: undefined,
+    help: "",
     legend: "",
     name: "",
   }
@@ -26,45 +28,72 @@ const onChange = (e: Event) => {
 }
 </script>
 <template>
-  <fieldset>
-    <InputLabel class="block" :label="legend" tag="legend"></InputLabel>
-    <label
-      class="inline-flex items-center"
-      :class="{ 'cursor-not-allowed': $attrs.disabled }"
-      :for="`${hasNameAttr ? name : uuid}-true`"
-    >
-      <input
-        type="radio"
-        class="w-4 h-4 border-gray-600 focus:ring-blue-500 text-xy-blue"
-        :id="`${hasNameAttr ? name : uuid}-true`"
-        :name="hasNameAttr ? name : uuid"
-        :value="true"
-        :checked="modelValue === true"
-        v-bind="{
-          ...$attrs,
-          onChange: onChange,
-        }"
-      />
-      <InputLabel class="ml-2" label="Yes" tag="span"></InputLabel>
-    </label>
-    <label
-      class="inline-flex items-center ml-6"
-      :class="{ 'cursor-not-allowed': $attrs.disabled }"
-      :for="`${hasNameAttr ? name : uuid}-false`"
-    >
-      <input
-        type="radio"
-        class="w-4 h-4 border-gray-600 focus:ring-blue-500 text-xy-blue"
-        :id="`${hasNameAttr ? name : uuid}-false`"
-        :name="hasNameAttr ? name : uuid"
-        :value="false"
-        :checked="modelValue === false"
-        v-bind="{
-          ...$attrs,
-          onChange: onChange,
-        }"
-      />
-      <InputLabel class="ml-2" label="No" tag="span"></InputLabel>
-    </label>
+  <fieldset
+    class="space-y-3"
+    :aria-labelledby="legend ? `${uuid}-legend` : undefined"
+    :aria-describedby="help ? `${uuid}-help` : undefined"
+  >
+    <div class="space-y-0.5">
+      <InputLabel
+        class="block my-auto"
+        :label="legend"
+        tag="legend"
+      ></InputLabel>
+      <InputHelp tag="p" :text="help" :id="`${uuid}-help`" />
+    </div>
+    <div>
+      <label
+        class="inline-flex items-center"
+        :class="{ 'cursor-not-allowed': $attrs.disabled }"
+        :for="`${hasNameAttr ? name : uuid}-true`"
+      >
+        <input
+          type="radio"
+          class="w-4 h-4 border-gray-600 focus:ring-blue-500 text-xy-blue disabled:opacity-50 disabled:cursor-not-allowed"
+          :id="`${hasNameAttr ? name : uuid}-true`"
+          :name="hasNameAttr ? name : uuid"
+          :value="true"
+          :checked="modelValue === true"
+          v-bind="{
+            ...$attrs,
+            onChange: onChange,
+          }"
+        />
+        <InputLabel
+          class="ml-2"
+          :disabled="
+            $attrs.hasOwnProperty('disabled') && $attrs.disabled !== false
+          "
+          label="Yes"
+          tag="span"
+        ></InputLabel>
+      </label>
+      <label
+        class="inline-flex items-center ml-6"
+        :class="{ 'cursor-not-allowed': $attrs.disabled }"
+        :for="`${hasNameAttr ? name : uuid}-false`"
+      >
+        <input
+          type="radio"
+          class="w-4 h-4 border-gray-600 focus:ring-blue-500 text-xy-blue disabled:opacity-50 disabled:cursor-not-allowed"
+          :id="`${hasNameAttr ? name : uuid}-false`"
+          :name="hasNameAttr ? name : uuid"
+          :value="false"
+          :checked="modelValue === false"
+          v-bind="{
+            ...$attrs,
+            onChange: onChange,
+          }"
+        />
+        <InputLabel
+          class="ml-2"
+          :disabled="
+            $attrs.hasOwnProperty('disabled') && $attrs.disabled !== false
+          "
+          label="No"
+          tag="span"
+        ></InputLabel>
+      </label>
+    </div>
   </fieldset>
 </template>
