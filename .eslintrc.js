61d129b79401c4690c80b23846ec781039be2fad
Steve Klein <steven@xyplanningnetwork.com>
2023-04-14 18:14:43 -0500

    refactor eslint, prettier configs, update readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,13 @@
 
 module.exports = {
   ignorePatterns: ["dist/**/*", "trees/**/*", "types/**/*"],
-  extends: "./eslint-config-base",
+  extends: "./config/eslint.js",
+  rules: {
+    "vue/attributes-order": 0,
+    "vue/attribute-hyphenation": 0,
+    "vue/v-on-event-hyphenation": 0,
+    "vue/no-v-html": 0,
+    "vue/no-template-shadow": 0,
+    "vue/require-explicit-emits": 0,
+  },
 }
