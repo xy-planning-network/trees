f6aca2b4f7b15f4de4b2ef9a1683cb5ba4c90caa
Joe Timmer <profsmallpine@gmail.com>
2023-06-14 16:23:00 -0600

    Improves popover edge case handling

--- src/lib-components/overlays/Tooltip.vue
+++ src/lib-components/overlays/Tooltip.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
 import Popover, { PopoverPosition } from "./Popover/Popover.vue"
 import { InformationCircleIcon } from "@heroicons/vue/outline"
+import { ref } from "vue"
 
+// props
 withDefaults(
   defineProps<{
     as?: string
@@ -13,26 +15,46 @@ withDefaults(
   }
 )
 
-const click = (event: MouseEvent): void => {
-  if (event.target) (event.target as HTMLElement).click()
+// refs
+const popoverHover = ref(false)
+const popoverTimeout = ref()
+
+// functions
+const closePopover = (close: any): void => {
+  popoverHover.value = false
+  if (popoverTimeout.value) clearTimeout(popoverTimeout.value)
+  popoverTimeout.value = setTimeout(() => {
+    if (!popoverHover.value) {
+      close()
+    }
+  }, 100)
+}
+
+const hoverPopover = (e: MouseEvent, open: boolean): void => {
+  popoverHover.value = true
+  if (!open && e.target) (e.target as HTMLElement).click()
 }
 </script>
 
 <template>
   <Popover :position="position" :as="as">
-    <template #button>
-      <div class="leading-none w-4 h-4" @mouseover="click($event)" @mouseleave="click($event)">
+    <template #button="{ open, close }">
+      <div class="leading-none w-4 h-4" @mouseover="hoverPopover($event, open)" @mouseleave="closePopover(close)">
         <InformationCircleIcon />
         <!--creates a larger clickable surface area 40 x 40-->
         <div
-          class="p-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        class="p-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
         ></div>
       </div>
     </template>
-    <div
-      class="w-full max-w-xs bg-white rounded-md px-3 py-2 border border-gray-100 drop-shadow-md text-xs text-gray-900 leading-snug font-medium"
-    >
-      <slot></slot>
-    </div>
+    <template #default="{open, close}">
+      <div
+        class="w-full max-w-xs bg-white rounded-md px-3 py-2 border border-gray-100 drop-shadow-md text-xs text-gray-900 leading-snug font-medium"
+        @mouseover.prevent="popoverHover = true"
+        @mouseleave.prevent="closePopover(close)"
+      >
+        <slot></slot>
+      </div>
+    </template>
   </Popover>
 </template>
