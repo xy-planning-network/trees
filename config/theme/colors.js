749c3b657ab3ec84205bdc64664089a591fc1eb8
Steve Klein <steven@xyplanningnetwork.com>
2023-08-31 13:09:54 -0500

    bump tailwind and add new brand colors and typegraphy to tailwind config

--- config/theme/colors.js
+++ config/theme/colors.js
@@ -1,28 +1,58 @@
 const colors = require("tailwindcss/colors")
 
-// these colors are deprecated
-// let's avoid using them
-delete colors["lightBlue"]
-delete colors["warmGray"]
-delete colors["trueGray"]
-delete colors["coolGray"]
-delete colors["blueGray"]
+export const brandColors = {
+  blue: {
+    50: "#C9E4F6",
+    100: "#B3D9F3",
+    200: "#81C2F7",
+    300: "#52A8FA",
+    400: "#268CFA",
+    500: "#1F6DF4",
+    600: "#2850BE", // xy-blue Brand:Blueberry
+    700: "#20399B",
+    800: "#161E74",
+    900: "#0C044E", // xy-indigo Brand:Indigo Girl
+  },
+  green: {
+    50: "#DEEFB7",
+    100: "#CBE698",
+    200: "#B7DD79", // xy-green Brand:Pistachio
+    300: "#9FCF6C",
+    400: "#86C25F",
+    500: "#6DB454",
+    600: "#51A749",
+    700: "#469946",
+    800: "#3B8A43",
+    900: "#307D41",
+  },
+  neutral: {
+    // holding on usage for potential palette update
+    50: "#FAF9F8",
+    100: "#E7E6E5",
+    200: "#C9C8C7",
+    300: "#B0AFAE",
+    400: "#979696",
+    500: "#7F7E7D",
+    600: "#666565",
+    700: "#4D4C4C",
+    800: "#333333", // xy-black Brand:XYPN Black
+    900: "#1C1B1B",
+  },
+}
 
 module.exports = {
-  ...colors,
   "xy-blue": {
-    DEFAULT: "#1F6DF4",
+    DEFAULT: brandColors["blue"][600],
+    ...brandColors["blue"],
+  },
+  "xy-indigo": {
+    DEFAULT: brandColors["blue"][900],
   },
   "xy-green": {
-    DEFAULT: "#51A749",
+    DEFAULT: brandColors["green"][200],
+    ...brandColors["green"],
   },
-  "xy-navy": {
-    DEFAULT: "#26547C",
-  },
-  "xy-dawn": {
-    DEFAULT: "#D1FAFF",
-  },
-  "xy-lime": {
-    DEFAULT: "#DEEFB7",
+  "xy-black": {
+    DEFAULT: brandColors["neutral"][800],
   },
 }
--- config/theme/fontFamily.js
+++ config/theme/fontFamily.js
@@ -1,4 +1,5 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
+  display: ["Work Sans", ...defaultTheme.fontFamily.sans],
   sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-};
+}
