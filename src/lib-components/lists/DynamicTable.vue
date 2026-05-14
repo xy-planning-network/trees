a147cf03126ef8eaa01393e2801cd3b1850cb917
Steve Klein <steven@xyplanningnetwork.com>
2026-05-14 16:02:51 -0500

    fix rendered space when no interface is present

--- src/lib-components/lists/DynamicTable.vue
+++ src/lib-components/lists/DynamicTable.vue
@@ -268,6 +268,7 @@ loadAndRender()
 <template>
   <div>
     <div
+      v-if="tableOptions.search || tableOptions.dateSearch"
       class="flex flex-col mb-4 space-y-4 lg:space-y-0 lg:flex-row lg:justify-between"
     >
       <div v-if="tableOptions.search" class="w-full max-w-lg lg:max-w-xs">
